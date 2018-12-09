5664883bb6822325906539b08fa2c29ff17c942b
Sammyt <sammy.teillet@gmail.com>
2018-12-09 14:35:03 +0100

    feat(): Add CommandBus

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Article } from './article.entity';
+import { CQRSModule } from '@nestjs/cqrs';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { Article } from './article.entity';
       synchronize: true,
     }),
     TypeOrmModule.forFeature([Article]),
+    CQRSModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,12 +3,15 @@ import { ArticleDTO } from './article.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Article } from './article.entity';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateArticleCommand } from './commands/implementations/create-article.command';
 
 @Injectable()
 export class AppService {
   constructor(
     @InjectRepository(Article)
     private readonly articleRepository: Repository<Article>,
+    private readonly commandBus: CommandBus,
   ) {}
 
   root(): string {
@@ -16,6 +19,8 @@ export class AppService {
   }
 
   async storeArticle(articleDto: ArticleDTO): Promise<Article> {
+    const createArticleCommand = new CreateArticleCommand(articleDto);
+    this.commandBus.execute(createArticleCommand);
     const article = await this.articleRepository.create(articleDto);
     await this.articleRepository.save(article);
     return article;
